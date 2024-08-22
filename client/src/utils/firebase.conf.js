0d11a2df6d7b0f29d07f0f695ee63fbdb530267f
Aryalekshmi G <aryaglekshmi@gmail.com>
2024-08-22 10:45:40 +0400

    my list page

--- client/src/utils/firebase.conf.js
+++ client/src/utils/firebase.conf.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
- apiKey: "AIzaSyApPAt0AoDnWDTIHkta2pUNlOXjHBb6M5Q",
- authDomain: "react-netflix-clone-3d959.firebaseapp.com",
- projectId: "react-netflix-clone-3d959",
- storageBucket: "react-netflix-clone-3d959.appspot.com",
- messagingSenderId: "758228196255",
- appId: "1:758228196255:web:c1786882c0a4eefa32fbaa",
- measurementId: "G-TMZ4G0DGH9"
+ apiKey: "xxxx",
+ authDomain: "xxx",
+ projectId: "xxx",
+ storageBucket: "xxxx",
+ messagingSenderId: "xx",
+ appId: "xxx",
+ measurementId: "xxxx"
 };
 
 // Initialize Firebase
