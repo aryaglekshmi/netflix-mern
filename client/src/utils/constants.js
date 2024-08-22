9db74d202e95b780f521473d5524e5a1413854d0
Aryalekshmi G <aryaglekshmi@gmail.com>
2024-08-22 11:12:39 +0400

    sample constant file added

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const API_KEY_MOVIE = "IMDB_API_KEY";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3/";
+export const API_URL = "http://localhost:1000/api/";
+export const FIREBASE_API = "FIREBASE_API_KEY";
