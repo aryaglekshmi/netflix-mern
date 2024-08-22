3030a6824fec674cac73c0ff53c6374d4a2466ab
Aryalekshmi G <aryaglekshmi@gmail.com>
2024-08-22 11:06:18 +0400

    Update .gitignore to ignore constants.js

--- client/src/utils/firebase.conf.js
+++ client/src/utils/firebase.conf.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { FIREBASE_API } from "../utils/constants";
 
 const firebaseConfig = {
- apiKey: "xxxx",
- authDomain: "xxx",
- projectId: "xxx",
- storageBucket: "xxxx",
- messagingSenderId: "xx",
- appId: "xxx",
- measurementId: "xxxx"
+  apiKey: FIREBASE_API,
+  authDomain: "react-netflix-clone-3d959.firebaseapp.com",
+  projectId: "react-netflix-clone-3d959",
+  storageBucket: "react-netflix-clone-3d959.appspot.com",
+  messagingSenderId: "758228196255",
+  appId: "1:758228196255:web:c1786882c0a4eefa32fbaa",
+  measurementId: "G-TMZ4G0DGH9",
 };
 
 // Initialize Firebase
